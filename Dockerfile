ef5c86f9b551444707874c0f7e59f71c2ceef4f1
Lukas Prettenthaler <rdx@wsn.at>
2021-02-15 11:59:02 +0100

    add custom entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM yandex/clickhouse-server:21.2.3.15
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eo pipefail
+
+CLICKHOUSE_UID="${CLICKHOUSE_UID:-"$(id -u clickhouse)"}"
+CLICKHOUSE_GID="${CLICKHOUSE_GID:-"$(id -g clickhouse)"}"
+
+gosu="gosu $USER:$GROUP"
+
+CLICKHOUSE_CONFIG="${CLICKHOUSE_CONFIG:-/etc/clickhouse-server/config.xml}"
+
+exec $gosu /usr/bin/clickhouse-server --config-file="$CLICKHOUSE_CONFIG"
