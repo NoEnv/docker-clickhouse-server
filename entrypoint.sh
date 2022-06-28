4c825c5f38668b6ce16c5c81da444d029d086155
Lukas Prettenthaler <lukas@noenv.com>
2022-06-28 15:22:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,17 @@ set -eo pipefail
 CLICKHOUSE_UID="${CLICKHOUSE_UID:-"$(id -u clickhouse)"}"
 CLICKHOUSE_GID="${CLICKHOUSE_GID:-"$(id -g clickhouse)"}"
 
-gosu="su-exec $CLICKHOUSE_UID:$CLICKHOUSE_GID"
+if [ "$(id -u)" = "0" ]; then
+    USER=$CLICKHOUSE_UID
+    GROUP=$CLICKHOUSE_GID
+else
+    USER="$(id -u)"
+    GROUP="$(id -g)"
+fi
 
 CLICKHOUSE_CONFIG="${CLICKHOUSE_CONFIG:-/etc/clickhouse-server/config.xml}"
 
-exec $gosu /usr/bin/clickhouse-server --config-file="$CLICKHOUSE_CONFIG"
+CLICKHOUSE_WATCHDOG_ENABLE=${CLICKHOUSE_WATCHDOG_ENABLE:-0}
+export CLICKHOUSE_WATCHDOG_ENABLE
+    
+/usr/bin/clickhouse su "${USER}:${GROUP}" /usr/bin/clickhouse-server --config-file="$CLICKHOUSE_CONFIG"
